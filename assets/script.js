74bd6924fa0ceacfba09ea559af924b54a99cb97
N-lara <n_lara01@yahoo.com>
2023-09-23 22:26:48 -0500

    added color coding for hours past present and future

--- assets/script.js
+++ assets/script.js
@@ -3,32 +3,37 @@
 // in the html.
 var schedule = $('#schedule')
 var today = dayjs();
-var hour = dayjs().format('HH');
+var hour = 15//dayjs().format('HH');
 var day = dayjs().format('DD');
-$('#currentDay').text('today is: '+ today.format('MMM DD, YYYY'));
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  //when the save button is clicked the text in its sibling text area is saved to local storage under the key named the same as the area id
   schedule.on('click', 'button', function(event){
     event.preventDefault();
     var text = $(event.target).prev('textarea').val();
     var key = $(event.target).parent('div').attr('id');
     localStorage.setItem(key, text);
   })
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  for(var i = 9; i <= 17; i++){
+    if(i < hour){
+      schedule.children('#hour-'+i).addClass('past');
+      schedule.children('#hour-'+i).removeClass('present future');
+    }else if(i > hour){
+      schedule.children('#hour-'+i).addClass('future');
+      schedule.children('#hour-'+i).removeClass('present past');
+    }else{
+    schedule.children('#hour-'+i).addClass('present');
+    schedule.children('#hour-'+i).removeClass('past future');
+    }
+  }
+
+  //gets saved information from local storage and puts the info into the schedule text areas of the corresponding hours
   for(var i = 9; i <= 17; i++){
     var check = 'hour-'+i
     var savedEvent = localStorage.getItem(check);
@@ -37,7 +42,8 @@ $(function () {
     }
     schedule.children('#hour-'+i).children('textarea').text(savedEvent);
   }
-  
+
+  //displays date on top of page
   $('#currentDay').text('today is: '+ today.format('MMM DD, YYYY'));
 });
 
